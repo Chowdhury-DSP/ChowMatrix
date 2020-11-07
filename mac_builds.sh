1da7a23b7c0f49a2f118b125cb8a2b8950fc7a32
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2020-11-06 18:43:01 -0800

    Add Mac beta builds

---
+++ mac_builds.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# clean up old builds
+rm -Rf build/
+rm -Rf bin/Mac/
+
+# set up build VST
+VST_PATH=~/Developer/AnalogTapeModel/Plugin/Juce/VST2_SDK/
+sed -i '' "8s~.*~juce_set_vst2_sdk_path(${VST_PATH})~" CMakeLists.txt
+sed -i '' '14s/#//' CMakeLists.txt
+
+# cmake new builds
+cmake -Bbuild -GXcode
+cmake --build build --config Release -j8
+
+# copy builds to bin
+mkdir -p bin/Mac
+declare -a plugins=("ChowMatrix")
+for plugin in "${plugins[@]}"; do
+    cp -R build/${plugin}_artefacts/Release/Standalone/${plugin}.app bin/Mac/${plugin}.app
+    cp -R build/${plugin}_artefacts/Release/VST/${plugin}.vst bin/Mac/${plugin}.vst
+    cp -R build/${plugin}_artefacts/Release/VST3/${plugin}.vst3 bin/Mac/${plugin}.vst3
+    cp -R build/${plugin}_artefacts/Release/AU/${plugin}.component bin/Mac/${plugin}.component
+done
+
+# reset CMakeLists.txt
+git restore CMakeLists.txt
