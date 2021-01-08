176323267935904aaed8b2d5783e49f67da583c5
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2021-01-07 23:31:45 -0800

    Extend tempo-sync delay to go from 1/32 to 2/1

--- src/dsp/Delay/DelaySyncUtils.h
+++ src/dsp/Delay/DelaySyncUtils.h
@@ -24,19 +24,26 @@ struct DelayRhythm
     double tempoFactor;
 };
 
-static constexpr std::array<DelayRhythm, 12> rhythms {
-    DelayRhythm ("Sixteenth Triplet", "1/16 T", 0.5 / 3.0 ),
-    DelayRhythm ("Sixteenth",         "1/16",   0.25      ),
-    DelayRhythm ("Sixteenth Dot",     "1/16 D", 0.25 * 1.5),
-    DelayRhythm ("Eigth Triplet",     "1/8 T",  1.0 / 3.0 ),
-    DelayRhythm ("Eigth",             "1/8",    0.5       ),
-    DelayRhythm ("Eigth Dot",         "1/8 D",  0.5 * 1.5 ),
-    DelayRhythm ("Quarter Triplet",   "1/4 T",  2.0 / 3.0 ),
-    DelayRhythm ("Quarter",           "1/4",    1.0       ),
-    DelayRhythm ("Quarter Dot",       "1/4 D",  1.0 * 1.5 ),
-    DelayRhythm ("Half Triplet",      "1/2 T",  4.0 / 3.0 ),
-    DelayRhythm ("Half",              "1/2",    2.0       ),
-    DelayRhythm ("Half Dot",          "1/2 D",  2.0 * 1.5 ),
+static constexpr std::array<DelayRhythm, 19> rhythms {
+    DelayRhythm ("Thirty-Second",     "1/32",   0.125       ),
+    DelayRhythm ("Thirty-Second Dot", "1/32 D", 0.125 * 1.5 ),
+    DelayRhythm ("Sixteenth Triplet", "1/16 T", 0.5 / 3.0   ),
+    DelayRhythm ("Sixteenth",         "1/16",   0.25        ),
+    DelayRhythm ("Sixteenth Dot",     "1/16 D", 0.25 * 1.5  ),
+    DelayRhythm ("Eigth Triplet",     "1/8 T",  1.0 / 3.0   ),
+    DelayRhythm ("Eigth",             "1/8",    0.5         ),
+    DelayRhythm ("Eigth Dot",         "1/8 D",  0.5 * 1.5   ),
+    DelayRhythm ("Quarter Triplet",   "1/4 T",  2.0 / 3.0   ),
+    DelayRhythm ("Quarter",           "1/4",    1.0         ),
+    DelayRhythm ("Quarter Dot",       "1/4 D",  1.0 * 1.5   ),
+    DelayRhythm ("Half Triplet",      "1/2 T",  4.0 / 3.0   ),
+    DelayRhythm ("Half",              "1/2",    2.0         ),
+    DelayRhythm ("Half Dot",          "1/2 D",  2.0 * 1.5   ),
+    DelayRhythm ("Whole Triplet",     "1/1 T",  8.0 / 3.0   ),
+    DelayRhythm ("Whole",             "1/1",    4.0         ),
+    DelayRhythm ("Whole Dot",         "1/1 D",  4.0 * 1.5   ),
+    DelayRhythm ("Two Whole Triplet", "2/1 T",  16.0 / 3.0  ),
+    DelayRhythm ("Two Whole",         "2/1",    8.0         ),
 };
 
 /** Return delay in seconds for rhythm and tempo */
