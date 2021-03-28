cfa99fdf10c9d276a96202ffa773143d79df202c
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2021-03-27 20:35:29 -0700

    Adjust pluginval

--- validate.sh
+++ validate.sh
@@ -36,7 +36,7 @@ fi
 
 echo "Pluginval installed at ${pluginval}"
 echo "Validating ${plugin}"
-$pluginval --strictness-level 8 --validate-in-process --validate $plugin --timeout-ms 180000
+$pluginval --strictness-level 8 --validate-in-process --validate $plugin --timeout-ms 240000
 result=$?
 
 # clean up
