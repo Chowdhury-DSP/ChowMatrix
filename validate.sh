e3aff70ae1d08434e87188aec911e851eb278383
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2021-04-01 09:55:12 -0400

    Extend pluginval timeout even more

--- validate.sh
+++ validate.sh
@@ -36,7 +36,7 @@ fi
 
 echo "Pluginval installed at ${pluginval}"
 echo "Validating ${plugin}"
-$pluginval --strictness-level 8 --validate-in-process --validate $plugin --timeout-ms 1200000
+$pluginval --strictness-level 8 --validate-in-process --validate $plugin --timeout-ms 1500000
 result=$?
 
 # clean up
