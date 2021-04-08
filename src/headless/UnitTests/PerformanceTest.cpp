a3b22a3f56d145cb6056c2896dee0fa27272a6b6
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2021-04-07 20:59:04 -0400

    Add param assign test

--- src/headless/UnitTests/PerformanceTest.cpp
+++ src/headless/UnitTests/PerformanceTest.cpp
@@ -11,7 +11,7 @@ public:
 
     void runTest() override
     {
-        constexpr double rtLimit = 250.0;
+        constexpr double rtLimit = 24.0;
 
         beginTest ("No Interp Test");
         runPerfTest (DelayType::NoInterp, rtLimit);
