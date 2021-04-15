36a18ca3f32cd27c1892bba5d008a4f17588d8ce
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2021-04-15 08:15:29 -0700

    Reset linker warning flags

--- modules/cmake/WarningFlags.cmake
+++ modules/cmake/WarningFlags.cmake
@@ -21,7 +21,7 @@ elseif((CMAKE_CXX_COMPILER_ID STREQUAL "Clang") OR (CMAKE_CXX_COMPILER_ID STREQU
         # Comment them out to be more strict.
         -Wno-shadow-field-in-constructor
         # Needed for ARM processor, OSX versions below 10.14
-        # -fno-aligned-allocation
+        -fno-aligned-allocation
     )
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     target_compile_options(warning_flags INTERFACE
