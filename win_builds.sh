bbdf5712bb3412b87e1551a90ccba4bff658feb2
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2021-04-16 17:57:46 -0700

    Fix Windows build script

--- win_builds.sh
+++ win_builds.sh
@@ -13,48 +13,48 @@ build32(){
 # exit on failure
 set -e
 
-# # clean up old builds
-# rm -Rf build/
-# rm -Rf build32/
-# rm -Rf bin/*Win64*
-# rm -Rf bin/*Win32*
+# clean up old builds
+rm -Rf build/
+rm -Rf build32/
+rm -Rf bin/*Win64*
+rm -Rf bin/*Win32*
 
-# # set up VST and ASIO paths
-# sed -i -e "9s/#//" CMakeLists.txt
-# sed -i -e "10s/#//" CMakeLists.txt
-# sed -i -e '16s/#//' CMakeLists.txt
+# set up VST and ASIO paths
+sed -i -e "9s/#//" CMakeLists.txt
+sed -i -e "10s/#//" CMakeLists.txt
+sed -i -e '16s/#//' CMakeLists.txt
 
-# # cmake new builds
-# build64 &
-# build32 &
-# wait
+# cmake new builds
+build64 &
+build32 &
+wait
 
-# # copy builds to bin
-# mkdir -p bin/Win64
-# mkdir -p bin/Win32
-# declare -a plugins=("ChowMatrix")
-# for plugin in "${plugins[@]}"; do
-#     cp -R build/${plugin}_artefacts/Release/Standalone/${plugin}.exe bin/Win64/${plugin}.exe
-#     cp -R build/${plugin}_artefacts/Release/VST/${plugin}.dll bin/Win64/${plugin}.dll
-#     cp -R build/${plugin}_artefacts/Release/VST3/${plugin}.vst3 bin/Win64/${plugin}.vst3
+# copy builds to bin
+mkdir -p bin/Win64
+mkdir -p bin/Win32
+declare -a plugins=("ChowMatrix")
+for plugin in "${plugins[@]}"; do
+    cp -R build/${plugin}_artefacts/Release/Standalone/${plugin}.exe bin/Win64/${plugin}.exe
+    cp -R build/${plugin}_artefacts/Release/VST/${plugin}.dll bin/Win64/${plugin}.dll
+    cp -R build/${plugin}_artefacts/Release/VST3/${plugin}.vst3 bin/Win64/${plugin}.vst3
 
-#     cp -R build32/${plugin}_artefacts/Release/Standalone/${plugin}.exe bin/Win32/${plugin}.exe
-#     cp -R build32/${plugin}_artefacts/Release/VST/${plugin}.dll bin/Win32/${plugin}.dll
-#     cp -R build32/${plugin}_artefacts/Release/VST3/${plugin}.vst3 bin/Win32/${plugin}.vst3
-# done
+    cp -R build32/${plugin}_artefacts/Release/Standalone/${plugin}.exe bin/Win32/${plugin}.exe
+    cp -R build32/${plugin}_artefacts/Release/VST/${plugin}.dll bin/Win32/${plugin}.dll
+    cp -R build32/${plugin}_artefacts/Release/VST3/${plugin}.vst3 bin/Win32/${plugin}.vst3
+done
 
-# # reset CMakeLists.txt
-# git restore CMakeLists.txt
+# reset CMakeLists.txt
+git restore CMakeLists.txt
 
-# # zip builds
-# VERSION=$(cut -f 2 -d '=' <<< "$(grep 'CMAKE_PROJECT_VERSION:STATIC' build/CMakeCache.txt)")
-# (
-#     cd bin
-#     rm -f "ChowMatrix-Win64-${VERSION}.zip"
-#     rm -f "ChowMatrix-Win32-${VERSION}.zip"
-#     zip -r "ChowMatrix-Win64-${VERSION}.zip" Win64
-#     zip -r "ChowMatrix-Win32-${VERSION}.zip" Win32
-# )
+# zip builds
+VERSION=$(cut -f 2 -d '=' <<< "$(grep 'CMAKE_PROJECT_VERSION:STATIC' build/CMakeCache.txt)")
+(
+    cd bin
+    rm -f "ChowMatrix-Win64-${VERSION}.zip"
+    rm -f "ChowMatrix-Win32-${VERSION}.zip"
+    zip -r "ChowMatrix-Win64-${VERSION}.zip" Win64
+    zip -r "ChowMatrix-Win32-${VERSION}.zip" Win32
+)
 
 # create installer
 echo "Creating installer..."
