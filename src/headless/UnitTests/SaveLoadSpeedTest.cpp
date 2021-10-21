c17a7257f6114c15e9f7bc9c2de890d83907d7cf
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2021-10-21 11:58:58 -0600

    Fix a bunch of little things

--- src/headless/UnitTests/SaveLoadSpeedTest.cpp
+++ src/headless/UnitTests/SaveLoadSpeedTest.cpp
@@ -57,7 +57,7 @@ public:
         std::cout << "Loaded state " << numIter << " times in " << duration << " seconds" << std::endl;
         std::cout << "Average time " << duration / (double) numIter << std::endl;
 
-        expectLessThan (duration, 2.0, "Loading state took too long!");
+        expectLessThan (duration, 3.0, "Loading state took too long!");
     }
 };
 
