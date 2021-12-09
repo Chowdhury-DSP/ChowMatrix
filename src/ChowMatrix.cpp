6ea8077ea324054f31b5036f7209034839756829
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2021-12-09 00:17:49 +0000

    Add option to only show Matrix or Details views

--- src/ChowMatrix.cpp
+++ src/ChowMatrix.cpp
@@ -140,18 +140,43 @@ AudioProcessorEditor* ChowMatrix::createEditor()
     builder->registerLookAndFeel ("BottomBarLNF", std::make_unique<BottomBarLNF>());
     builder->registerLookAndFeel ("PresetsLNF", std::make_unique<PresetsLNF>());
 
+#if JUCE_IOS
+    builder->setIdsToIgnore ({ "NodeDetails" });
+#endif
+
     // GUI trigger functions
-    magicState.addTrigger ("flush_delays", [=] {
-        NodeManager::doForNodes (&inputNodes, [] (DelayNode* n) { n->flushDelays(); });
-    });
+    magicState.addTrigger ("flush_delays", [=]
+                           { NodeManager::doForNodes (&inputNodes, [] (DelayNode* n)
+                                                      { n->flushDelays(); }); });
 
-    magicState.addTrigger ("randomise", [=] {
-        NodeManager::doForNodes (&inputNodes, [] (DelayNode* n) { n->randomiseParameters(); });
-    });
+    magicState.addTrigger ("randomise", [=]
+                           { NodeManager::doForNodes (&inputNodes, [] (DelayNode* n)
+                                                      { n->randomiseParameters(); }); });
 
-    magicState.addTrigger ("insanity_reset", [=] {
-        insanityControl.resetInsanityState();
-    });
+    magicState.addTrigger ("insanity_reset", [=]
+                           { insanityControl.resetInsanityState(); });
+
+    auto changeUIComponentsToIgnore = [=] (std::initializer_list<Identifier> ids)
+    {
+        if (auto* editor = dynamic_cast<foleys::MagicPluginEditor*> (getActiveEditor()))
+        {
+            editor->getGUIBuilder().setIdsToIgnore (std::move (ids));
+            editor->setConfigTree (editor->getGUIBuilder().getConfigTree());
+        }
+    };
+
+    magicState.addTrigger ("view_control",
+                           [=]
+                           {
+                               PopupMenu menu;
+                               menu.addItem ("Only show Matrix View", [=]
+                                             { changeUIComponentsToIgnore ({ "NodeDetails" }); });
+                               menu.addItem ("Only show Details View", [=]
+                                             { changeUIComponentsToIgnore ({ "GraphView" }); });
+                               menu.addItem ("Show both views", [=]
+                                             { changeUIComponentsToIgnore ({}); });
+                               menu.showMenuAsync (PopupMenu::Options());
+                           });
 
 #if ! JUCE_IOS
     auto editor = new foleys::MagicPluginEditor (magicState, BinaryData::gui_xml, BinaryData::gui_xmlSize, std::move (builder));
--- src/gui/BottomBar/BottomBarLNF.cpp
+++ src/gui/BottomBar/BottomBarLNF.cpp
@@ -96,16 +96,13 @@ void BottomBarLNF::positionComboBoxText (ComboBox& box, Label& label)
 void BottomBarLNF::drawButtonBackground (Graphics& g,
                                          Button& button,
                                          const Colour& backgroundColour,
-                                         bool shouldDrawButtonAsHighlighted,
-                                         bool shouldDrawButtonAsDown)
+                                         bool /*shouldDrawButtonAsHighlighted*/,
+                                         bool /*shouldDrawButtonAsDown*/)
 {
     auto bounds = button.getLocalBounds().toFloat().reduced (0.5f, 0.5f);
 
     auto baseColour = backgroundColour.withMultipliedSaturation (button.hasKeyboardFocus (true) ? 1.3f : 0.9f)
                           .withMultipliedAlpha (button.isEnabled() ? 1.0f : 0.5f);
-
-    if (shouldDrawButtonAsDown || shouldDrawButtonAsHighlighted)
-        baseColour = baseColour.contrasting (shouldDrawButtonAsDown ? 0.2f : 0.05f);
 
     g.setColour (baseColour);
     g.fillRect (bounds);
